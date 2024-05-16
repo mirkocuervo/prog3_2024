3b696db8796a330a76348e90f017fc6fb0b4fea7
Mirko Pereyra <pmirko298@gmail.com>
2024-05-15 21:21:55 -0300

    mi primer commit

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Primer Commit</title>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste sequi exercitationem saepe aliquid quaerat quam deleniti voluptas ea earum magnam quasi, culpa officiis. Incidunt est molestias, soluta nemo optio nobis.
+</head>
+<body>
+    
+</body>
+</html>
